3623ece61e24d615a379fa7ada17d83c16adb88a
Pedro <ppls2106@gmail.com>
2019-04-03 11:11:02 -0300

    03/04

--- Main.py
+++ Main.py
@@ -1,4 +1,33 @@
-def renomear():
-    print('Programa de teste')
+import Grafo
+grafo = {"a" : ["c"],
+         "b" : ["c","e"],
+         "c" : ["a","b","d","e"],
+         "d" : ["c"],
+         "e" : ["c","b"],
+         "f" : []
+        }
 
-renomear()
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+    # return edges
+    print("vertices conexos: ", edges)
+
+def find_isolated_nodes(graph):
+    """ returns a list of isolated nodes. """
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    # return isolated
+    print("vertices isolados:", isolated)
+
+# print(generate_edges(grafo))
+# print(find_isolated_nodes(grafo))
+# generate_edges(grafo)
+# find_isolated_nodes(grafo)
+
+graph = Grafo(g)
+print(graph.vertices())
