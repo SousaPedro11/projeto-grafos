a9c2ef9b1728f3f29e5acc8ac80a2b2b63ec917f
Pedro <ppls2106@gmail.com>
2019-04-17 11:39:27 -0300

    edit gitignore

--- Trabalho_final/graph.py
+++ Trabalho_final/graph.py
@@ -83,4 +83,3 @@ graph1.verificar_eureliano()
 graph1.caminho_curto()
 print("Caminho mais curto entre os vétices 'a' e 'b' para grafo valorado, usando Dijkstra")
 print(graph3.shortest_path("a","b"))
-teste
